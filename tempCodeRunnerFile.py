849a672b08d82ad094326eefd4e2b8504f28cb19
Jimmy Truong <risenalpha@gmail.com>
2023-05-01 14:56:57 -0600

    Untracker coderunner file

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-        # Draw the frequency of the current key on the bottom right of the screen
-                            font = pygame.font.SysFont('Calibri', 20, True, False)
-                            screen.blit(font.render(str(freq
